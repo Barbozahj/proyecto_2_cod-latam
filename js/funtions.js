b8cecb40cd0823e994e1d20a7c4029ab20e575c7
Proyecto_2_cod-latam <barbozahj@gmail.com>
2023-07-23 18:12:17 -0300

    Two Commit

--- js/funtions.js
+++ js/funtions.js
@@ -1,9 +1,14 @@
-
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+function toggleMenu() {
+  const menu = document.querySelector('.topnav ul');
+  const icon = document.querySelector('.topnav .icon');
+  const logo = document.querySelector('.topnav .logo');
+  if (window.innerWidth <= 600) { // Modo hamburguesa
+    menu.classList.toggle('show-nav');
+    const isMenuShown = menu.classList.contains('show-nav');
+    icon.innerHTML = isMenuShown ? '<i class="fa fa-times"></i>' : '<i class="fa fa-bars"></i>';
+  } else { // Modo desktop
+    menu.classList.remove('show-nav');
+    icon.innerHTML = '<i class="fa fa-bars"></i>';
+    logo.classList.toggle('opposite');
   }
 }
